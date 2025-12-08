b19810ff9b9ac03787148ddeb961201d1d0e10b7
Smit <136905857+Smit093@users.noreply.github.com>
2025-12-08 14:29:04 +0530

    Update Footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -8,14 +8,11 @@ export default function Footer() {
       <div className="flex flex-col items-center">
         <p className="m-0">&copy; {new Date().getFullYear()} <i>Market Minds</i>. All rights reserved.</p>
 <p className="mt-2">Made by 
-  <Link href="https://smitdev.netlify.app/" className="text-blue-500 hover:underline ml-1">Smit Patel</Link>
+  <Link href="https://smitdev.vercel.app/" className="text-blue-500 hover:underline ml-1">Smit Patel</Link>
 </p>
 
         <div className="mt-2 flex gap-4">
-          <a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="text-white text-2xl hover:text-gray-300">
-            <FaFacebookF />
-          </a>
-          <a href="https://www.instagram.com/justsmittt/?next=%2F" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="text-white text-2xl hover:text-gray-300">
+          <a href="https://www.instagram.com/smiit.0709/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="text-white text-2xl hover:text-gray-300">
             <FaInstagram />
           </a>
           <a href="https://x.com/justsmittt" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="text-white text-2xl hover:text-gray-300">
