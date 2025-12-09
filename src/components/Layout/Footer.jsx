795abd8f61054ae9578e59f8c54712f9b4720c73
Smit <smitpatel9265@gmail.com>
2025-12-09 12:25:14 +0530

    Update Footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaInstagram, FaTwitter, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-5 text-center font-poppins">
       <div className="flex flex-col items-center">
         <p className="m-0">&copy; {new Date().getFullYear()} <i>Market Minds</i>. All rights reserved.</p>
-<p className="mt-2">Made by 
-  <Link href="https://smitdev.vercel.app/" className="text-blue-500 hover:underline ml-1">Smit Patel</Link>
-</p>
+        <p className="mt-2">Made by
+          <a href="https://smitdev.vercel.app/" target="_blank"
+            className="text-blue-500 hover:underline ml-1">Smit Patel</a>
+        </p>
 
         <div className="mt-2 flex gap-4">
           <a href="https://www.instagram.com/smiit.0709/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="text-white text-2xl hover:text-gray-300">
